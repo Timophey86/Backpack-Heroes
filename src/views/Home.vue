ec5a8439fd98ac99c765cdd8e2ea995921c092d6
ShiranCohen86 <shiranc86@gmail.com>
2021-03-17 22:39:50 +0200

    user details page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,38 @@
 <template>
-  <div class="home-page">
+  <section class="home-page">
     <h1>Home Page</h1>
-  </div>
+    <div class="hero">
+      <h3>Hero Photo Home Page</h3>
+    </div>
+    <div class="category">
+      <h3>List of categories</h3>
+    </div>
+    <div class="location">
+      <h3>List of locations</h3>
+    </div>
+    <div class="reviews">
+      <h3>List of reviews</h3>
+    </div>
+    <div class="about-vision">
+      <h3>About our vision</h3>
+    </div>
+  </section>
 </template>
-
+<script>
+export default {
+  name: "homePage",
+  data() {
+    return {
+      projs: [],
+    };
+  },
+  methods: {
+    loadProjs() {
+      this.projs = this.$store.getters.projs;
+    },
+  },
+  created() {
+    this.loadProjs();
+  },
+};
+</script>
