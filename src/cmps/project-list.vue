c2d9a0bbcadc6fe66ba737c7c01c2fffe353bb3b
ShiranCohen86 <shiranc86@gmail.com>
2021-03-17 18:50:48 +0200

    preview component

---
+++ src/cmps/project-list.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="project-list">
+    <li>
+      <project-preview />
+    </li>
+  </ul>
+</template>
+<script>
+import projectPreview from "@/cmps/project-preview.vue";
+export default {
+  name: "projectList",
+  components: {
+    projectPreview,
+  },
+};
+</script>
--- src/views/project-app.vue
+++ src/views/project-app.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="project-app-page">
     <h1>Project App Page</h1>
+    <project-list/>
   </div>
 </template>
+<script>
+import ProjectList from '../cmps/project-list.vue';
+
+export default {
+  name: "projectApp",
+  components: {
+    ProjectList,
+  },
+};
+</script>
