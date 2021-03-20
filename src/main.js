0079333cc675a58a0f6e1d93cb95601152cd47eb
ShiranCohen86 <shiranc86@gmail.com>
2021-03-20 18:12:37 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -6,10 +6,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import './assets/main.scss'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.use(ElementUI);
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
