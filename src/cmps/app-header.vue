523d5b56b70856276f354b8386c4b30abffb2d01
Dariya-karetko <dariya93k@gmail.com>
2021-03-27 11:36:28 +0300

    css

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -4,17 +4,20 @@
       <div class="logo">
         <router-link to="/">Backpack Heroes</router-link>
       </div>
-    <div class="nav">
-      <router-link to="/project">Explore</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/login-signup">Login/Signup</router-link>
-      <router-link v-if="currUser" class="user-link" to="/user/userId=1"
-        ><i class="fas fa-user-circle"></i
-      > {{currUser.fullname}}</router-link>
-      <router-link v-else class="user-link" to="/user/userId=1"
-        ><i class="fas fa-user-circle"></i
-      ></router-link>
-    </div>
+
+      <div class="menu" @click="toggleMenu"><i class="fas fa-bars"></i></div>
+
+      <nav :class="menuClass">
+        <router-link to="/project">Explore</router-link>
+        <router-link to="/login-signup">Login/Signup</router-link>
+        <router-link v-if="currUser" class="user-link" to="/user/userId=1"
+          ><i class="fas fa-user-circle"></i
+          >{{ currUser.fullname }}</router-link
+        >
+        <router-link v-else class="user-link" to="/user/userId=1"
+          ><i class="fas fa-user-circle"></i
+        ></router-link>
+      </nav>
     </div>
   </div>
 </template>
@@ -23,14 +26,22 @@
 export default {
   data() {
     return {
-    }
+      menuClass: "closeMenu",
+    };
   },
   computed: {
     currUser() {
-      return this.$store.getters.loggedinUser
-    }
+      return this.$store.getters.loggedinUser;
+    },
   },
   methods: {
+    toggleMenu() {
+      if (this.menuClass === "openMenu") {
+        this.menuClass = "closeMenu";
+      } else {
+        this.menuClass = "openMenu";
+      }
+    },
   },
 };
 </script>
