d1415b2a9d4290ae112ad1c5f7ff042ea220ad73
Dariya-karetko <dariya93k@gmail.com>
2021-03-27 11:44:16 +0300

    css

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -4,7 +4,6 @@
       <div class="logo">
         <router-link to="/">Backpack Heroes</router-link>
       </div>
-<<<<<<< HEAD
 
       <div class="menu" @click="toggleMenu"><i class="fas fa-bars"></i></div>
 
@@ -19,19 +18,6 @@
           ><i class="fas fa-user-circle"></i
         ></router-link>
       </nav>
-=======
-      <div class="nav">
-        <router-link to="/project">Explore</router-link>
-        <router-link to="/about">About</router-link>
-        <router-link to="/login-signup">Login/Signup</router-link>
-        <router-link v-if="currUser" class="user-link" to="/user/userId=1">
-          {{ currUser.fullname }} <el-avatar :src="avatarImg" :size="25"/>
-          </router-link>
-        <router-link v-else to="/login-signup"
-          ><i class="fas fa-user-circle"></i
-        ></router-link>
-      </div>
->>>>>>> 52a681c843989f3af35bd73e346be477b2ac76c7
     </div>
   </div>
 </template>
@@ -39,13 +25,11 @@
 <script>
 export default {
   data() {
-<<<<<<< HEAD
+
     return {
       menuClass: "closeMenu",
     };
-=======
-    return {};
->>>>>>> 52a681c843989f3af35bd73e346be477b2ac76c7
+
   },
   computed: {
     avatarImg() {
@@ -54,7 +38,6 @@ export default {
     currUser() {
       return this.$store.getters.loggedinUser;
     },
-<<<<<<< HEAD
   },
   methods: {
     toggleMenu() {
@@ -64,9 +47,7 @@ export default {
         this.menuClass = "openMenu";
       }
     },
-=======
->>>>>>> 52a681c843989f3af35bd73e346be477b2ac76c7
+
   },
-  methods: {},
 };
 </script>
